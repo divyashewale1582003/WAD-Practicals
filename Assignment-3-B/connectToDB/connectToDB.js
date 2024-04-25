e213394945c25f5a858e4ad66c7f5b44fa26b069
kunal1516 <147371834+kunal1516@users.noreply.github.com>
2024-04-25 11:06:08 +0530

    new change

--- Assignment-3-B/connectToDB/connectToDB.js
+++ Assignment-3-B/connectToDB/connectToDB.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 
-const URL = 'mongodb+srv://Gaurav-WAD:oeOCnPDwbArJB3vg@cluster0.nq1mdcw.mongodb.net/'
+const URL =
+  "mongodb+srv://Kunal-WAD:oeOCnPDwbArJB3vg@cluster0.nq1mdcw.mongodb.net/";
 
-export const connectToDB = () =>{
-    mongoose.connect(URL)
-    console.log("Connected to DB")
-}
+export const connectToDB = () => {
+  mongoose.connect(URL);
+  console.log("Connected to DB");
+};
